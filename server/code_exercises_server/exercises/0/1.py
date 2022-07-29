c3d2159639aad49963048b2fac8d14a8496856ee
Jonathan Breidfjord <jbreidfjord@gmail.com>
2022-07-29 12:49:22 -0600

    demo exercise

---
+++ server/code_exercises_server/exercises/0/1.py
@@ -0,0 +1,14 @@
+"""
+Write a function that returns the sum of two integer values, a and b.
+"""
+
+
+def sum(a: int, b: int) -> int:
+    """
+    Calculates the sum of two integers
+
+    Args:
+        a (int): Value to be added
+        b (int): Value to be added
+    """
+    ...  # To be implemented by the student
